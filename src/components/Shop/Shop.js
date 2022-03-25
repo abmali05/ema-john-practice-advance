e353e566018b054518350eecc625109c1f23e04d
abmali05 <bestofhonest@gmail.com>
2022-03-25 10:48:44 +0600

    final

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDb } from '../../utilities/fakedb';
 import './Shop.css';
 
 const Shop = () => {
@@ -22,6 +24,7 @@ const Shop = () => {
         const newCart = [...cart, product];
         // console.log(newCart);
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
@@ -38,9 +41,8 @@ const Shop = () => {
 
             </div>
             <div className="cart-container">
-                <h2>This is Order</h2>
-                <p>Selected Items: {cart.length} </p>
 
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
